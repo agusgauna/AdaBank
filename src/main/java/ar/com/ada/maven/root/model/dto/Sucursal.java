3f223bb02865b46c5314e5e3e2b6bc3dcde10f13
Romina <rominar2008@gmail.com>
2020-02-03 21:32:14 -0300

    creacion rama BankDAO

--- src/main/java/ar/com/ada/maven/root/model/dto/Sucursal.java
+++ src/main/java/ar/com/ada/maven/root/model/dto/Sucursal.java
@@ -3,9 +3,57 @@ package ar.com.ada.maven.root.model.dto;
 public class Sucursal {
 
 
+    private int id;
+    private String name;
+    private int code;
+    private Bank bankId;
 
     public Sucursal() {
     }
 
+    public Sucursal(int id, String name, int code, Bank bankId) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.bankId = bankId;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Bank getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(Bank bankId) {
+        this.bankId = bankId;
+    }
+
+    @Override
+    public String toString() {
+        return "Sucursal{" + "id=" + id + ", name='" + name + '\'' +
+                ", code=" + code + ", bankId=" + bankId +
+                '}';
+    }
 }

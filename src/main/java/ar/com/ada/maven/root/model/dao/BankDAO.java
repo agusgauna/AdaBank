bb993f01407e43968a003325f7501be56f86b89b
Romina <rominar2008@gmail.com>
2020-02-03 21:51:45 -0300

    se agrega dao de bank-sin terminar

--- src/main/java/ar/com/ada/maven/root/model/dao/BankDAO.java
+++ src/main/java/ar/com/ada/maven/root/model/dao/BankDAO.java
@@ -1,5 +1,35 @@
 package ar.com.ada.maven.root.model.dao;
 
-public class BankDAO  {
-    
+import ar.com.ada.maven.root.model.DBConection;
+import ar.com.ada.maven.root.model.dto.Account;
+import ar.com.ada.maven.root.model.dto.Bank;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class BankDAO implements DAO<Bank>{
+
+    private Boolean willCloseConnection = true;
+
+    public BankDAO() {
+    }
+    public AccountDAO(Boolean willCloseConnection) {
+        this.willCloseConnection = willCloseConnection;
+    }
+
+
+    @Override
+    public ArrayList<Bank> findAll() {
+        String sql = "SELECT * FROM Bank";
+
+        ArrayList<Account> bancos = new ArrayList<>();
+        try{
+        Connection connection = DBConection.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                
+            }
 }

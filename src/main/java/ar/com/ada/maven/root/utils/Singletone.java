24c6369de1251bab8609ce2d1c8cee3be6a5f802
Agustina <agustinagauna@gmail.com>
2020-02-10 20:19:22 -0300

    Se agrega ClientView

--- src/main/java/ar/com/ada/maven/root/utils/Singletone.java
+++ src/main/java/ar/com/ada/maven/root/utils/Singletone.java
@@ -1,11 +1,15 @@
 package ar.com.ada.maven.root.utils;
 
+import ar.com.ada.maven.root.view.MainView;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Singletone {
     private static Scanner scanner;
 
-    private Singletone() {}
+    private Singletone() {
+    }
 
     public static Scanner getInstance() {
         if (scanner == null) {
@@ -21,5 +25,41 @@ public class Singletone {
         } catch (Exception e) {
             System.out.println("ERROR MESSAGE: " + e.getMessage());
         }
+    }
+
+    public static String getInputString() {
+        Scanner scanner = getInstance();
+        while (true) {
+            try {
+                String txt = scanner.nextLine().trim();
+                while (!txt.matches("^[A-Za-záéíóúüÁÉÍÓÚÜ\\s]+$") && !txt.isEmpty()) {
+                    MainView.invalidData();
+                    txt = scanner.nextLine();
+                }
+                return txt;
+            } catch (InputMismatchException e) {
+                MainView.invalidData();
+                scanner.next();
+            }
+        }
+    }
+
+    public static String getInputInteger() {
+        Scanner scanner = getInstance();
+        while (true) {
+            try {
+                System.out.print("? ");
+                String txt = scanner.nextLine().trim();
+                while (!txt.matches("^[0-9]+$") && !txt.isEmpty()) {
+                    MainView.invalidData();
+                    txt = scanner.nextLine();
+                }
+                return txt;
+            } catch (InputMismatchException e) {
+                MainView.invalidData();
+                scanner.next();
+            }
+        }
+
     }
 }
